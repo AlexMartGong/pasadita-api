7fc0f3dc7eaeacb7d66e2c46011df5a5192f0a47
AlexMartGong <155708055+AlexMartGong@users.noreply.github.com>
2025-03-30 23:49:56 -0600

    Initial configuration of the Spring Boot project

---
+++ src/test/java/com/pasadita/api/PasaditaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasadita.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasaditaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
