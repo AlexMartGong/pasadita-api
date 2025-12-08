4ca2ec748cdf85e536ad86672ae03009bda4572d
AlexMartGong <te210111518@tequila.tecmm.edu.mx>
2025-12-08 00:05:56 -0600

    Add new fields to SaleResponseDto: employeePhone, customerPhone, saleDetails, deliveryOrderId, and deliveryAddress

--- src/main/java/com/pasadita/api/dto/sale/SaleResponseDto.java
+++ src/main/java/com/pasadita/api/dto/sale/SaleResponseDto.java
@@ -1,5 +1,6 @@
 package com.pasadita.api.dto.sale;
 
+import com.pasadita.api.dto.saledetail.SaleDetailResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -16,8 +18,10 @@ public class SaleResponseDto {
     private Long id;
     private Long employeeId;
     private String employeeName;
+    private String employeePhone;
     private Long customerId;
     private String customerName;
+    private String customerPhone;
     private Long paymentMethodId;
     private String paymentMethodName;
     private LocalDateTime datetime;
@@ -26,4 +30,7 @@ public class SaleResponseDto {
     private BigDecimal total;
     private boolean paid;
     private String notes;
+    private List<SaleDetailResponseDto> saleDetails;
+    private Long deliveryOrderId;
+    private String deliveryAddress;
 }
