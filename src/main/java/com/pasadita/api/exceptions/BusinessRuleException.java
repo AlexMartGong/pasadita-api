7006285435c28a13af6fb6babe24a61780c24a09
Alex <te210111518@tequila.tecmm.edu.mx>
2026-04-24 22:33:40 -0600

    Add custom exception handling with BusinessRuleException and EntityNotFoundException

---
+++ src/main/java/com/pasadita/api/exceptions/BusinessRuleException.java
@@ -0,0 +1,8 @@
+package com.pasadita.api.exceptions;
+
+public class BusinessRuleException extends RuntimeException {
+
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
